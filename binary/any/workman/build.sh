0fc1a01ff567963ce10112bb8f223d69574d0493
Pjotr Prins <pjotr.public01@thebird.nl>
2011-10-08 16:04:56 +0200

    Uploader

--- binary/any/workman/build.sh
+++ binary/any/workman/build.sh
@@ -4,3 +4,4 @@ fakeroot dpkg-deb --build debian
 export version=`dpkg-deb -f debian.deb Version`
 export package=`dpkg-deb -f debian.deb Package`
 mv -v debian.deb "$package"_"$version"_all.deb 
+lintian "$package"_"$version"_all.deb
---
+++ binary/any/workman/upload.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+rsync --progress *.deb $x bio4.dnsalias.net:/export2/httpd/download/Debian/$dir
+
